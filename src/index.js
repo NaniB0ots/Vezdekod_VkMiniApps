09460ca28ff35bcde17a2871748b15e7c6645535
Алексей <alexleskov2000@gmail.com>
2021-05-22 15:29:58 +0800

    Установить пакеты для VKUI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { AppRoot } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
