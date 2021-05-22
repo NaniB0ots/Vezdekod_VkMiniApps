0ae29137e814f5fbd3c35b8e703f1d328af8af96
Алексей <alexleskov2000@gmail.com>
2021-05-22 16:55:27 +0800

    Изменить карточки команды

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -10,7 +10,7 @@ import {
     PanelHeader,
     SimpleCell,
     View,
-    CardScroll
+    CardScroll, Avatar, RichCell, Button
 } from "@vkontakte/vkui";
 
 export default function TeamCard() {
@@ -19,58 +19,60 @@ export default function TeamCard() {
             <Panel id="main">
                 <PanelHeader>NaniB0ots</PanelHeader>
 
-                <Group mode="plain" header={<Header mode="primary" subtitle="ИБб <3">
-                    Команда Иркутского национального исследовательского технического университета
+
+                <Group  header={<Header mode="primary" subtitle="ИБб <3">
+                    Команда Иркутского политеха
                 </Header>}>
-                    <CardGrid size="m">
-                        <ContentCard
-                            image="https://sun4-16.userapi.com/s/v1/ig2/pgJfaAMN6nq9ADZCpmTqScRDimS6Htg0svTui0XD6JFKyaQT1vxOIYrNwqcto9_ZoRNlBWLpAOfgZqdir5acm4Ux.jpg?size=200x0&quality=96&crop=2,255,582,582&ava=1"
-                            subtitle="Возраст: 20"
-                            header="Сидоренко Анжелика Витальевна"
-                            text="Занимается всем визуалом, проектированием пользовательских интерфейсов и сценариев.
+                    <RichCell
+                        disabled
+                        multiline
+                        before={<Avatar size={150}
+                                        src="https://sun4-16.userapi.com/s/v1/ig2/pgJfaAMN6nq9ADZCpmTqScRDimS6Htg0svTui0XD6JFKyaQT1vxOIYrNwqcto9_ZoRNlBWLpAOfgZqdir5acm4Ux.jpg?size=200x0&quality=96&crop=2,255,582,582&ava=1"/>}
+                        text="Занимается всем визуалом, проектированием пользовательских интерфейсов и сценариев.
                             (Гений фигмы, а еще ее невозможно выйграть в монополию)"
-                            caption="Специализация: UI/UX-дизайн"
-                            maxHeight={300}
-                        />
-                        <ContentCard
-                            image="https://sun4-10.userapi.com/s/v1/ig2/qCRLzOldNBEX8Hg_rRvM77W6k4GzeZFIlUVdvq2YdIVGllYdinlX6Vmy6ikmz0J7fnjBmLKGW5okw-uIu4kAjPb5.jpg?size=200x0&quality=96&crop=2,111,583,583&ava=1"
-                            subtitle="Возраст: 21"
-                            header="Ашимов Султан Сапарович"
-                            text="Помимо роли разработчика, выполняет функции менеджера команды. (Чисто разнорабочий микрочелик и капитан по-совместительству)"
-                            caption="Специализация: Backend-разработчик"
-                            maxHeight={300}
-                        />
-                    </CardGrid>
-                    <CardGrid size="m">
-                        <ContentCard
-                            image="https://sun4-15.userapi.com/s/v1/if1/-t08ZbXWujc3iJCHDjvSAvCueHWaTZo8l3IFdwLarbz0kVALei-eRXdKhyUl5HoMv5FexMYO.jpg?size=200x0&quality=96&crop=720,80,1022,1531&ava=1"
-                            subtitle="Возраст: 21"
-                            header="Лесков Алексей Олегович"
-                            text="Главный гений-кодинга в команде, определяет весь технический стек и архитектуру проектов, которые реализует команда."
-                            caption="Специализация: Fullstack-разработчик"
-                            maxHeight={300}
-                        />
+                        caption="Специализация: UI/UX-дизайн"
+                        bottom="Возраст: 20"
+                    >
+                        Сидоренко Анжелика Витальевна
+                    </RichCell>
+                    <RichCell
+                        disabled
+                        multiline
+                        before={<Avatar size={150}
+                                        src="https://sun4-10.userapi.com/s/v1/ig2/qCRLzOldNBEX8Hg_rRvM77W6k4GzeZFIlUVdvq2YdIVGllYdinlX6Vmy6ikmz0J7fnjBmLKGW5okw-uIu4kAjPb5.jpg?size=200x0&quality=96&crop=2,111,583,583&ava=1"/>}
+                        text="Помимо роли разработчика, выполняет функции менеджера команды.
+                        (Чисто разнорабочий микрочелик и капитан по-совместительству)"
+                        caption="Специализация: Backend-разработчик"
+                        bottom="Возраст: 21"
+                    >
+                        Ашимов Султан Сапарович
+                    </RichCell>
 
-                        <ContentCard
-                            image="https://newsland.com/static/u/article_image/17/07/31/tmpYfbaIF.jpeg"
-                            subtitle="Возраст: 21"
-                            header="Черемных Руслан Максимович"
-                            text="Тот самый человек, который шарит в математике и нейросетях."
-                            caption="Специализация: DataScience"
-                            maxHeight={300}
-                        />
+                    <RichCell
+                        disabled
+                        multiline
+                        before={<Avatar size={150}
+                                        src="https://sun4-15.userapi.com/s/v1/if1/-t08ZbXWujc3iJCHDjvSAvCueHWaTZo8l3IFdwLarbz0kVALei-eRXdKhyUl5HoMv5FexMYO.jpg?size=200x0&quality=96&crop=720,80,1022,1531&ava=1"/>}
+                        text="Главный гений-кодинга в команде, определяет весь технический стек и архитектуру проектов,
+                            которые реализует команда."
+                        caption="Специализация: Fullstack-разработчик"
+                        bottom="Возраст: 21"
+                    >
+                        Лесков Алексей Олегович
+                    </RichCell>
 
-                    </CardGrid>
+                    <RichCell
+                        disabled
+                        multiline
+                        before={<Avatar size={150}
+                                        src="https://newsland.com/static/u/article_image/17/07/31/tmpYfbaIF.jpeg"/>}
+                        text="Тот самый человек, который шарит в математике и нейросетях."
+                        caption="Специализация: DataScience"
+                        bottom="Возраст: 21"
+                    >
+                        Черемных Руслан Максимович
+                    </RichCell>
                 </Group>
-            </Panel>
-            <Panel id="greetings">
-                aba1
-            </Panel>
-            <Panel id="form">
-                aba2
-            </Panel>
-            <Panel id="finish">
-                aba3
             </Panel>
         </View>
     )
